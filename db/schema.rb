8991e6faf0d5e237ef71b7bb490fb1d453ad4dfe
Kristin Beltz <beltz08@me.com>
2015-07-23 13:55:40 -0500

    Add due date column to TodoItem table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723184425) do
+ActiveRecord::Schema.define(version: 20150723185439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150723184425) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "todo_list_id"
+    t.date     "due_date"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
