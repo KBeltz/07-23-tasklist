4536fe8a7e4b6cd0ef22dc79a676eea55e1892fb
Kristin Beltz <beltz08@me.com>
2015-07-23 13:07:22 -0500

    Add database relationships.

--- db/migrate/20150723175457_create_todo_items.rb
+++ db/migrate/20150723175457_create_todo_items.rb
@@ -4,6 +4,7 @@ class CreateTodoItems < ActiveRecord::Migration
       t.string :task
       t.string :assignee
       t.boolean :status
+      t.todo_list_id :integer
 
       t.timestamps null: false
     end
--- db/migrate/20150723180032_create_todo_lists.rb
+++ db/migrate/20150723180032_create_todo_lists.rb
@@ -3,6 +3,7 @@ class CreateTodoLists < ActiveRecord::Migration
     create_table :todo_lists do |t|
       t.string :title
       t.text :description
+      t.user_id :integer
 
       t.timestamps null: false
     end
